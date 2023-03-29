750a6f660dea89592496ee211ced14c0f85a3c6a
azim0203 <shaikhazim7048@gmail.com>
2023-03-30 00:48:50 +0530

    This is a new commit for what I originally planned to be amended

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ header.style.borderBottom='solid 3px #000';
 const item=document.querySelector('.title')
 item.style.fontweight='bold';
 item.style.color='green';
+const items=document.getElementsByClassName('list-group-item');
+items[2].style.backgroundColor='green';
+for(let i=0;i<items.length;i++){
+    items[i].style.fontWeight='bold';
+}
