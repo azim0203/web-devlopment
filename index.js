68d9670af2ab9fd5f74816218e392b637ca0ccaf
azim0203 <shaikhazim7048@gmail.com>
2023-03-30 01:16:34 +0530

    did it using getelemntbytag

--- index.js
+++ index.js
@@ -8,3 +8,5 @@ items[2].style.backgroundColor='green';
 for(let i=0;i<items.length;i++){
     items[i].style.fontWeight='bold';
 }
+const li=document.getElementsByTagName('li');
+li[4].style.backgroundColor='red';
