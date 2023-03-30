cc2ac5b670102750a408e2f9a51320359cb2ad31
azim0203 <shaikhazim7048@gmail.com>
2023-03-30 18:21:50 +0530

    used querryselector

--- index.js
+++ index.js
@@ -10,3 +10,14 @@ for(let i=0;i<items.length;i++){
 }
 const li=document.getElementsByTagName('li');
 li[4].style.backgroundColor='red';
+const background=document.querySelector('.list-group-item:nth-child(2)');
+background.style.backgroundColor='green';
+const dis=document.querySelector('.list-group-item:nth-child(3)');
+dis.style.display='none';
+const green=document.querySelectorAll('.list-group-item');
+green[1].style.color='green';
+const greenbackground=document.querySelectorAll('li:nth-child(odd)');
+for(let i=0;i<greenbackground.length;i++){
+    greenbackground[i].style.backgroundColor='green';
+
+}
