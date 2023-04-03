3ec2be44b69f8e8b5b65d6205c02db37df8a6f20
azim0203 <shaikhazim7048@gmail.com>
2023-04-03 23:36:15 +0530

    localstorage

---
+++ main.js
@@ -0,0 +1,8 @@
+let form=document.getElementById('my-form');
+form.addEventListener('submit',addvalue);
+function addvalue(e){
+    e.preventDefault();
+    let name=document.getElementById('name').value;
+    let email=document.getElementById('email').value;
+    localStorage.setItem(name,email);
+}
