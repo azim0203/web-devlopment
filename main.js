531cbd2b8aa08b75788dc15e3bb22c3231381fdc
azim0203 <shaikhazim7048@gmail.com>
2023-04-04 23:47:24 +0530

    added mutiple users

--- main.js
+++ main.js
@@ -12,4 +12,44 @@ function addvalue(e){
     let users=JSON.parse(localStorage.getItem('users'))||[];
     users.push(obj);
     localStorage.setItem('users',JSON.stringify(users));
+    displayUsers();
+
 }
+function displayUsers() {
+    const userList = document.getElementById('users');
+  
+    
+    let users = JSON.parse(localStorage.getItem('users')) || [];
+  
+    
+    userList.innerHTML = '';
+  
+    
+    users.forEach(function(user, index) {
+      const li = document.createElement('li');
+      li.innerHTML = `
+        ${user.name} (${user.email})
+        <button class="delete" data-index="${index}">Delete</button>
+      `;
+      userList.appendChild(li);
+    });
+  
+    
+    const deleteButtons = document.querySelectorAll('.delete');
+    deleteButtons.forEach(function(button) {
+      button.addEventListener('click', function(event) {
+        const index = event.target.dataset.index;
+  
+        
+        let users = JSON.parse(localStorage.getItem('users')) || [];
+        users.splice(index, 1);
+        localStorage.setItem('users', JSON.stringify(users));
+  
+        
+        event.target.parentElement.remove();
+      });
+    });
+  }
+  
+  
+  displayUsers();
