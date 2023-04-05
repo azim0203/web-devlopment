21bee214c75af55aa70c9d65b017f20a11b4ae64
azim0203 <shaikhazim7048@gmail.com>
2023-04-05 13:42:19 +0530

    added edit functionality

--- main.js
+++ main.js
@@ -1,19 +1,30 @@
 let form=document.getElementById('my-form');
 form.addEventListener('submit',addvalue);
 function addvalue(e){
-    e.preventDefault();
-    let name=document.getElementById('name').value;
-    let email=document.getElementById('email').value;
-    const obj={
-        name,
-        email
-    }
-    //localStorage.setItem('userdetails',JSON.stringify(obj));
-    let users=JSON.parse(localStorage.getItem('users'))||[];
-    users.push(obj);
-    localStorage.setItem('users',JSON.stringify(users));
-    displayUsers();
+  e.preventDefault();
+  let name=document.getElementById('name').value;
+  let email=document.getElementById('email').value;
+  const obj={
+      name,
+      email
+  }
 
+  let users=JSON.parse(localStorage.getItem('users'))||[];
+  
+  const editindex=form.dataset.editindex;
+  if(editindex !==undefined){
+    users[editindex].name=name;
+    users[editindex].email=email;
+    form.removeAttribute('data-edit-index');
+  }
+  else{
+    users.push(obj);
+  }
+  
+  localStorage.setItem('users',JSON.stringify(users));
+  displayUsers();
+  document.getElementById('name').value='';
+  document.getElementById('email').value='';
 }
 function displayUsers() {
     const userList = document.getElementById('users');
@@ -28,13 +39,23 @@ function displayUsers() {
     users.forEach(function(user, index) {
       const li = document.createElement('li');
       li.innerHTML = `
-        ${user.name} (${user.email})
+        <span class="name">${user.name}</span>
+         <span class="email">${user.email}</span>
+         <button class="edit" data-index="${index}">Edit</button>
         <button class="delete" data-index="${index}">Delete</button>
       `;
       userList.appendChild(li);
     });
-  
-    
+        const editButtons=document.querySelectorAll('.edit');
+        editButtons.forEach(function(button){
+          button.addEventListener('click',function(event){
+          const index=event.target.dataset.index;
+          let users=JSON.parse(localStorage.getItem('users'))||[];
+          const user=users[index];
+          document.getElementById('name').value=user.name;
+          document.getElementById('email').value=user.email;
+        form.dataset.editindex=index;        })})
+
     const deleteButtons = document.querySelectorAll('.delete');
     deleteButtons.forEach(function(button) {
       button.addEventListener('click', function(event) {
@@ -44,12 +65,14 @@ function displayUsers() {
         let users = JSON.parse(localStorage.getItem('users')) || [];
         users.splice(index, 1);
         localStorage.setItem('users', JSON.stringify(users));
-  
         
-        event.target.parentElement.remove();
+       displayUsers();
+        
+        
+        
       });
     });
   }
   
   
-  displayUsers();
+  
