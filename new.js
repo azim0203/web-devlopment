482efd38aafb4e9cdc7cd4bf399c2a60f01fe1ce
azim0203 <129212393+azim0203@users.noreply.github.com>
2023-05-16 01:08:53 +0530

    Add files via upload

--- new.js
+++ new.js
@@ -3,7 +3,7 @@ const table = document.getElementById('appointments').querySelector('tbody');
 
 // function to get appointments from server and populate the table
 function getAppointments() {
-  axios.get('https://crudcrud.com/api/e1cd423c257044a69bded75ab47557a1/appointments')
+  axios.get('https://crudcrud.com/api/bff3d48173bc4eaa998d7dc2ec34a91b/appointments')
     .then(response => {
       // clear the table before adding new rows
       table.innerHTML = '';
@@ -14,8 +14,13 @@ function getAppointments() {
           <td>${appointment.name}</td>
           <td>${appointment.email}</td>
           <td>${appointment.phone}</td>
+          <td><button class="delete-button">Delete</button></td>
         `;
       });
+      table.querySelectorAll('.delete-button').forEach(button=>{
+        button.addEventListener('click',handleDeleteClick)
+    })
+    
     })
     .catch(error => console.error(error));
 }
@@ -35,7 +40,7 @@ function handleSubmit(event) {
   };
 
   // POST the new appointment to the server
-  axios.post('https://crudcrud.com/api/e1cd423c257044a69bded75ab47557a1/appointments', appointment)
+  axios.post('https://crudcrud.com/api/bff3d48173bc4eaa998d7dc2ec34a91b/appointments', appointment)
     .then(response => {
       // add a new row to the table for the newly created appointment
       const row = table.insertRow();
@@ -43,8 +48,11 @@ function handleSubmit(event) {
         <td>${response.data.name}</td>
         <td>${response.data.email}</td>
         <td>${response.data.phone}</td>
+        <td><button class="delete-button">Delete</button></td>
+
       `;
       // reset the form
+      row.querySelector('.delete-button').addEventListener('click',handleDeleteClick)
       form.reset();
     })
     .catch(error => console.error(error));
@@ -52,3 +60,15 @@ function handleSubmit(event) {
 
 // add event listener to the form
 form.addEventListener('submit', handleSubmit);
+function handleDeleteClick(event){
+  event.preventDefault();
+  const row =event.target.closest('tr')
+  const id= row.dataset.id;
+  axios.delete(`https://crudcrud.com/api/bff3d48173bc4eaa998d7dc2ec34a91b/appointments/${id}`)
+  .then(res => {
+    row.remove();
+  })
+  .catch(err => {
+    console.error(err); 
+  })
+}
